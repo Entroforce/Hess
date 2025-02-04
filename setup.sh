dd38ecf650091ba4f77ed5151dea17d4fa042d04
Dmitry Pavlov <dpavlov@entroforce.ru>
2025-02-04 19:36:18 +0300

    setup.sh error check

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git submodule init &&
 git submodule update &&
-cp indigo/indigo_version.h indigo/Indigo/api/c/indigo/src/
+cp indigo/indigo_version.h indigo/Indigo/api/c/indigo/src/ &&
 cd hess-preparation && make CONF=Release && cd .. &&
 cd hess-empirical-docking && make CONF=Release && cd .. &&
 cd hess-se-docking && make CONF=Release && cd ..
